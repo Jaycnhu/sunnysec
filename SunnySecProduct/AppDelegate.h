47415f75d99aa4953c8d484c3556e0d8f0400b19
Huuuuuuj <huuuuuuj@zfmmdeMacBook-Air.local>
2017-12-14 09:32:34 +0800

    Initial Commit

---
+++ SunnySecProduct/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SunnySecProduct
+//
+//  Created by Huuuuuuj on 2017/12/14.
+//  Copyright © 2017年 SunnySec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
